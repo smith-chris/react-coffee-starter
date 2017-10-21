4d65819d9f471575bc68ee48c113a568ff87cfec
smith-chris <kowalski.k@outlook.com>
2017-10-21 18:29:21 +0100

    Most basic webpack-dev-server configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,22 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
   }
 }
