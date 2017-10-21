ae18eb0f3670e6e01e3ad3c4153a92afbb58acd1
smith-chris <kowalski.k@outlook.com>
2017-10-21 18:42:42 +0100

    Added htmlWebpackPlugin for automatic index.html generation from template and cleanWebpackPlugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -9,6 +11,13 @@ module.exports = {
   devServer: {
     contentBase: path.resolve(__dirname, 'dist')
   },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      inject: 'body'
+    })
+  ],
   module: {
     rules: [
       {
