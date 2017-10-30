89cd867314b5224cb792cfe4063e8e70096dc547
smith-chris <kowalski.k@outlook.com>
2017-10-30 13:23:30 +0000

    Improved styles a bit.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,14 +6,14 @@ import {
 } from 'react-router-dom'
 
 import Main from 'views/Main'
-import Second from 'views/Second'
+import Other from 'views/Other'
 
 const Routes = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Main}/>
-        <Route exact path="/second" component={Second}/>
+        <Route exact path="/other" component={Other}/>
       </Switch>
     </Router>
   )
--- src/views/Main/Main.js
+++ src/views/Main/Main.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
+import styles from './Main.sass'
 
 import {
   incrementCounter,
   decrementCounter
 } from 'store/actions/counterActions'
+
 import Button from 'components/Button'
+import Header from 'components/Header'
 
 const Main = props => {
   const {counter, increment, decrement} = props
   return (
-    <div>
-      <h1>Main view</h1>
-      <p>{counter}</p>
-      <Link to='/second'>
+    <div className={styles.main}>
+      <Header>Main view</Header>
+      <Link to='/other'>
         <Button/>
       </Link>
+      <p>{counter}</p>
       <div>
         <Button
           text='decrement!'
